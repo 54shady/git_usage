3b35a4bea7bb31c6d5305173772e9a00b970c677
zeroway <M_O_Bz@163.com>
2019-03-22 20:36:52 +0000

    Change email address
        run script
        ./change_eamil.sh
    
        if error raise as below
        Cannot create a new backup. A previous backup already exists in
        refs/original/ Force overwriting the backup with -f
    
        run command in shell:
    	git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch Rakefile' HEAD
    
        run script again
        ./change_eamil.sh
    
        final:
        git push origin --force --all
        git push origin --force --tags

---
+++ change_email.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="you_old_email@qq.com"
+CORRECT_NAME="new_name"
+CORRECT_EMAIL="new_email@google.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+	export GIT_COMMITTER_NAME="$CORRECT_NAME"
+	export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+	export GIT_AUTHOR_NAME="$CORRECT_NAME"
+	export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
