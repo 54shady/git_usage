60034256bdeb155dae1ebb6e20be9925e268bc80
zeroway <M_O_Bz@163.com>
2022-05-22 07:46:41 +0800

    add xray

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,17 @@ iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 MDN="vps.zeroway.xyz"
 ~/.acme.sh/acme.sh --issue -d $MDN --standalone
 ~/.acme.sh/acme.sh --installcert -d $MDN --key-file /root/private.key --fullchain-file /root/cert.crt
-bash <(curl -Ls https://raw.githubusercontent.com/54shady/git_usage/master/install.sh)
+#bash <(curl -Ls https://raw.githubusercontent.com/54shady/git_usage/master/install.sh)
+mkdir -p /usr/local/bin
+
+# https://github.com/XTLS/Xray-core/releases/tag/v1.5.5
+DOWNLOAD_PATH="https://raw.githubusercontent.com/54shady/git_usage/master"
+curl -Ls $DOWNLOAD_PATH/xray -o /usr/local/bin/xray
+curl -Ls $DOWNLOAD_PATH/geoip.dat -o /usr/local/bin/geoip.dat
+curl -Ls $DOWNLOAD_PATH/geosite.dat -o /usr/local/bin/geosite.dat
+curl -Ls $DOWNLOAD_PATH/config.json -o /usr/local/bin/config.json
+/usr/local/bin/xray -c /usr/local/bin/config.json &
+
 iptables -I INPUT -p tcp --dport 8888 -j ACCEPT
 iptables -I INPUT -p tcp --dport 443 -j ACCEPT
 
@@ -18,8 +28,8 @@ cat > /var/www/html/index.html << EOF
 <h1>Hello Sara</h1>
 EOF
 
-curl -Ls https://raw.githubusercontent.com/54shady/git_usage/master/caddy.service -o /lib/systemd/system/caddy.service
-ln -s /lib/systemd/system/caddy.service /etc/systemd/system/multi-user.target.wants/caddy.service
+#curl -Ls https://raw.githubusercontent.com/54shady/git_usage/master/caddy.service -o /lib/systemd/system/caddy.service
+#ln -s /lib/systemd/system/caddy.service /etc/systemd/system/multi-user.target.wants/caddy.service
 
 #echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" \
 #	    | tee -a /etc/apt/sources.list.d/caddy-fury.list
@@ -27,8 +37,9 @@ ln -s /lib/systemd/system/caddy.service /etc/systemd/system/multi-user.target.wa
 #systemctl stop caddy
 #rm -rf /usr/bin/caddy
 
-curl -Ls https://raw.githubusercontent.com/54shady/git_usage/master/caddy -o /usr/bin/caddy
+curl -Ls $DOWNLOAD_PATH/caddy -o /usr/bin/caddy
 chmod +x /usr/bin/caddy
 mkdir /etc/caddy
-curl -Ls https://raw.githubusercontent.com/54shady/mygentoo/master/vps/Caddyfile -o /etc/caddy/Caddyfile
-systemctl start caddy
+MYGTVPS="https://raw.githubusercontent.com/54shady/mygentoo/master/vps"
+curl -Ls $MYGTVPS/Caddyfile -o /etc/caddy/Caddyfile
+/usr/bin/caddy run --environ --config /etc/caddy/Caddyfile &
