ba9aa266a6173c79708ac92fd10eebd53c422606
zeroway <M_O_Bz@163.com>
2022-09-22 06:41:57 +0800

    rename dns host name to yieyu

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # this script work for debian 10
-#bash <(curl -Ls https://raw.githubusercontent.com/54shady/git_usage/master/deploy.sh vps.name.com)
+#bash <(curl -Ls https://raw.githubusercontent.com/54shady/git_usage/master/deploy.sh) vps.yieyu.xyz
 
 apt update -y && apt install -y curl socat
 curl https://get.acme.sh | sh -s email=zeroway5405@qq.com
 iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 
-# My DND Name: for example vps.name.xyz
+# My DND Name: for example vps.yieyu.xyz
 MDN="$1"
 ~/.acme.sh/acme.sh --issue -d $MDN --standalone
 ~/.acme.sh/acme.sh --installcert -d $MDN --key-file /root/private.key --fullchain-file /root/cert.crt
