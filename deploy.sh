ef18331c95ebce6713f2beaaef5b346e2bfa54fb
zeroway <M_O_Bz@163.com>
2022-11-02 04:55:04 +0800

    do some rename

--- deploy.sh
+++ deploy.sh
@@ -3,33 +3,33 @@
 # this script work for debian 10
 #bash <(curl -Ls https://raw.githubusercontent.com/54shady/git_usage/master/deploy.sh) abc.yieyu.xyz
 
+# Basic setup
 apt update -y && apt install -y curl socat
 curl https://get.acme.sh | sh -s email=zeroway5405@qq.com
-iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 
-# My DND Name: for example abc.yieyu.xyz
-MDN="$1"
-~/.acme.sh/acme.sh --force --issue -d $MDN --standalone --server LetsEncrypt.org
-~/.acme.sh/acme.sh --installcert -d $MDN --key-file /root/private.key --fullchain-file /root/cert.crt
+# Ports Configuration
+iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+iptables -I INPUT -p tcp --dport 443 -j ACCEPT
+
+# Fetch my domain name form first argument
+DOMAINAME="$1"
+~/.acme.sh/acme.sh --force --issue -d $DOMAINAME --standalone --server LetsEncrypt.org
+~/.acme.sh/acme.sh --installcert -d $DOMAINAME --key-file /root/private.key --fullchain-file /root/cert.crt
+
 #bash <(curl -Ls https://raw.githubusercontent.com/54shady/git_usage/master/install.sh)
 mkdir -p /usr/local/bin
 
 # https://github.com/XTLS/Xray-core/releases/tag/v1.5.5
-DOWNLOAD_PATH="https://raw.githubusercontent.com/54shady/git_usage/master"
-curl -Ls $DOWNLOAD_PATH/xray -o /usr/local/bin/xray
+SCRIPT_REPO="https://raw.githubusercontent.com/54shady/git_usage/master"
+curl -Ls $SCRIPT_REPO/xray -o /usr/local/bin/xray
 chmod +x /usr/local/bin/xray
-curl -Ls $DOWNLOAD_PATH/geoip.dat -o /usr/local/bin/geoip.dat
-curl -Ls $DOWNLOAD_PATH/geosite.dat -o /usr/local/bin/geosite.dat
-curl -Ls $DOWNLOAD_PATH/config.json -o /usr/local/bin/config.json
-curl -Ls $DOWNLOAD_PATH/trojan.json -o /usr/local/bin/trojan.json
+curl -Ls $SCRIPT_REPO/geoip.dat -o /usr/local/bin/geoip.dat
+curl -Ls $SCRIPT_REPO/geosite.dat -o /usr/local/bin/geosite.dat
+curl -Ls $SCRIPT_REPO/config.json -o /usr/local/bin/config.json
+curl -Ls $SCRIPT_REPO/trojan.json -o /usr/local/bin/trojan.json
 /usr/local/bin/xray -c /usr/local/bin/trojan.json &
 
-# no need for 8888 port open
-#iptables -I INPUT -p tcp --dport 8888 -j ACCEPT
-
-iptables -I INPUT -p tcp --dport 443 -j ACCEPT
-
-# install caddy
+# Caddy Setup
 mkdir -p /var/www/html
 cat > /var/www/html/index.html << EOF
 <h1>Hello Sara</h1>
@@ -44,14 +44,13 @@ EOF
 #systemctl stop caddy
 #rm -rf /usr/bin/caddy
 
-curl -Ls $DOWNLOAD_PATH/caddy -o /usr/bin/caddy
+curl -Ls $SCRIPT_REPO/caddy -o /usr/bin/caddy
 chmod +x /usr/bin/caddy
 mkdir /etc/caddy
-MYGTVPS="https://raw.githubusercontent.com/54shady/git_usage/master"
-curl -Ls $MYGTVPS/Caddyfile -o /etc/caddy/Caddyfile
+curl -Ls $SCRIPT_REPO/Caddyfile -o /etc/caddy/Caddyfile
 /usr/bin/caddy run --environ --config /etc/caddy/Caddyfile &
 
 # add a auto start script
-curl -Ls $DOWNLOAD_PATH/rc.local -o /etc/rc.local
+curl -Ls $SCRIPT_REPO/rc.local -o /etc/rc.local
 chmod +x /etc/rc.local
 ln -s /lib/systemd/system/rc.local.service /etc/systemd/system/
